28378ece63910ecc4b0345f3d727d68f4c377ca3
rkr <ron.kirschler@gmail.com>
2026-01-14 17:49:49 +0100

    Add `MCPToolRawProperty` class with schema validation and JSON serialization

---
+++ src/Common/Properties/MCPToolRawProperty.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace McpSrv\Common\Properties;
+
+use InvalidArgumentException;
+use McpSrv\Types\Tools\MCPToolProperty;
+
+/**
+ * @phpstan-type TRawPropertySchema object
+ */
+readonly class MCPToolRawProperty implements MCPToolProperty {
+	private object $schema;
+
+	/**
+	 * @param TRawPropertySchema|array<mixed> $schema
+	 */
+	public function __construct(
+		private string $name,
+		object|array $schema,
+		private bool $required = false,
+	) {
+		$this->schema = (object) $schema;
+		if(!property_exists($this->schema, 'type') || !is_string($this->schema->type)) {
+			throw new InvalidArgumentException('Property schema must include a string "type" key');
+		}
+	}
+
+	public function getName(): string {
+		return $this->name;
+	}
+
+	public function isRequired(): bool {
+		return $this->required;
+	}
+
+	/**
+	 * @return TRawPropertySchema
+	 */
+	public function jsonSerialize(): object {
+		$schema = (array) $this->schema;
+		$schema['required'] = $schema['required'] ?? $this->required;
+
+		return (object) $schema;
+	}
+}
