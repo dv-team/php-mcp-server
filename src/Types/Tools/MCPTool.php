e0a81a42fa70c8779456d8294a27a2ee1e4dffab
rkr <ron.kirschler@gmail.com>
2026-01-14 12:46:47 +0100

    Add optional returnSchema support to MCPTool and JSON serialization

--- src/Types/Tools/MCPTool.php
+++ src/Types/Tools/MCPTool.php
@@ -10,6 +10,7 @@ class MCPTool implements JsonSerializable {
 	 * @param string $description
 	 * @param MCPToolInputSchema $arguments
 	 * @param callable(object): MCPToolResult $handler
+	 * @param null|array<string, mixed> $returnSchema
 	 */
 	public function __construct(
 		public readonly string $name,
@@ -17,20 +18,28 @@ class MCPTool implements JsonSerializable {
 		public readonly MCPToolInputSchema $arguments,
 		public readonly bool $isDangerous,
 		public $handler,
+		public readonly ?array $returnSchema = null,
 	) {}
 	
 	/**
 	 * @return array{
 	 *     name: string,
 	 *     description: string,
-	 *     inputSchema: array<string, mixed>
+	 *     inputSchema: array<string, mixed>,
+	 *     returnSchema?: array<string, mixed>
 	 * }
 	 */
 	public function jsonSerialize(): array {
-		return [
+		$result = [
 			'name' => $this->name,
 			'description' => $this->description,
 			'inputSchema' => $this->arguments->jsonSerialize(),
 		];
+
+		if($this->returnSchema !== null) {
+			$result['returnSchema'] = $this->returnSchema;
+		}
+
+		return $result;
 	}
 }
